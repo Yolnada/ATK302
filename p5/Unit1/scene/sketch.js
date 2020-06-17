5e078a2d3d91fa870d23c271d5cdba71245d43d3
Yolanda <1004946881@163.com>
2020-06-18 00:31:35 +0800

    rising run

--- p5/Unit1/scene/sketch.js
+++ p5/Unit1/scene/sketch.js
@@ -1,21 +1,25 @@
 function setup() {
-  createCanvas(500, 500);
-  
+  createCanvas(400,400);
 }
 
 function draw() {
-  background(100) ; 
-  // put your shapes here!
-  
-  
-  text(mouseX + " , " + mouseY, 10, 20) ;  // this helps know where the mouse is
-} 
+  background(173,66,50);
+  noStroke();
 
-  
+  fill(224,227,210);
+  circle(200,150,50);
 
-function mouseReleased() { 
-  // if you click the mouse, you'll see the location in the debugging console
-  print(mouseX + " , " + mouseY) ; 
-} 
+  fill(33,30,40);
+  quad(150,220,250,220,400,400,0,400);
 
- 
+  stroke(255,128,128);
+  strokeWeight(5);
+  fill(224,227,210);
+  ellipse(100,100,100,100);
+
+  stroke(0);
+  line(0,0,100,100);
+  arc(200,50,50,50,PI,TWO_PI);
+  arc(150,50,50,50,PI,TWO_PI);
+
+}
